373aa3217ea58d34218bcc1e7d0a11323630ad0e
vfreis09 <reis.vicente27@gmail.com>
2024-11-22 16:46:57 -0300

    task struct created

--- main.c
+++ main.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char *task;
+    int completed;
+} Task;
+
+Task *tasks = NULL;
+int length = 0;
+
 
 int main(void){
     printf("hello world\n");
